617151d00e49e41c2f311b86a0bf547305078fe6
api-dmi-180440 <armando.munoz@utxicotepec.edu.mx>
2021-11-08 16:38:51 -0600

    master

--- src/database.js
+++ src/database.js
@@ -1,20 +1,11 @@
 const mysql = require('mysql');
 
-const mysqlConnection =  mysql.createConnection({
+const mysqlConnection =  mysql.createPool({
     host: 'byjtegxht44daenmhuv8-mysql.services.clever-cloud.com',
     port: '3306',
     user: 'uvgyzdh23of6df1f',
     password: 'cgYoOZAKBxokAh4D9G8S',
     database: 'byjtegxht44daenmhuv8'
-});
-
-mysqlConnection.connect(function (err){
-    if(err){
-        console.log(err);
-        return;
-    }else{
-        console.log('DB esta conectada');
-    }
 });
 
 module.exports  = mysqlConnection;
